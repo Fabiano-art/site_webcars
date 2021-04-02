b98dfb1305b8f7a3b60fb6b3d5f18d0108f41899
Fabiano Guimarães <fabio.dub@outlook.com>
2021-04-02 15:47:40 -0300

    Cabecalho melhorado

--- js/cabecalhoRetratil.js
+++ js/cabecalhoRetratil.js
@@ -1,10 +1,16 @@
-document.addEventListener("scroll", function() {
-    let posicao = window.pageYOffset;
-    // console.log(posicao)
-    if (posicao >= 300){
-        document.querySelector('header').style.visibility = "hidden"
+document.addEventListener("scroll", function(e) {
+    const posicao = 300
+    if (window.pageYOffset >= posicao){
+        document.querySelector('header').style.height = '50px'
+        document.querySelector('header img').style.width = '60px'
+        document.querySelector('.cabecalho').style.paddingTop = '0px'
     }
-    else {
-        document.querySelector('header').style.visibility = "visible"
+    else{
+        document.querySelector('header').style.height = '100px'
+        document.querySelector('header img').style.width = '120px'
+        document.querySelector('.cabecalho').style.paddingTop = '20px'
     }
 });
+
+// window.pageYOffset;
+
