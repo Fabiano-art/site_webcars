4c0f9e1f6753dc6aae26ce01c6035dfafedb1332
Fabiano Guimarães <fabio.dub@outlook.com>
2021-04-06 22:38:29 -0300

    Página sedans

--- js/cabecalhoRetratil.js
+++ js/cabecalhoRetratil.js
@@ -1,14 +1,20 @@
 document.addEventListener("scroll", function(e) {
-    const posicao = 300
+    const posicao = 200
     if (window.pageYOffset >= posicao){
         document.querySelector('header').style.height = '50px'
         document.querySelector('header img').style.width = '60px'
         document.querySelector('.cabecalho').style.paddingTop = '0px'
+        document.querySelectorAll('.menu-button li').forEach(e => {
+            e.style.fontSize = "20px"
+        })
     }
     else{
         document.querySelector('header').style.height = '100px'
         document.querySelector('header img').style.width = '120px'
         document.querySelector('.cabecalho').style.paddingTop = '20px'
+        document.querySelectorAll('.menu-button li').forEach(e => {
+            e.style.fontSize = "25px"
+        })
     }
 });
 
